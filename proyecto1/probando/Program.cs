a651a6cf10c8aa588cbcc12dc9e4dccfe5e1b84e
Sebastian Celeita <sceleita16@gmail.com>
2025-01-08 11:01:50 -0500

    NewKey

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string stripe_api_key = ""
+string stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
